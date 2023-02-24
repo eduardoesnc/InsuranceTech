c5da8df6be0fea408a9b3d507832a08c454e1e1b
atlascipri <atlasciprianoo@gmail.com>
2023-02-24 04:13:57 -0300

    'Redefinição de Senha FirebaseAuth'

--- lib/pages/cadastro.page.dart
+++ lib/pages/cadastro.page.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:insurancetech/models/checagem.model.dart';
-
 import '../components/largeButton.dart';
 import '../components/pageTitle.dart';
 
@@ -57,6 +56,11 @@ class _CadastroPageState extends State<CadastroPage> {
               child: TextFormField(
                 //autofocus: true,
                 controller: _nomeController,
+                validator:(value){
+                  if(value!.isEmpty){
+                    return 'Campo obrigatório';
+                  }
+                },
                 onChanged: (text) {},
                 keyboardType: TextInputType.text,
                 obscureText: false,
--- lib/pages/esqueceuSenhaEnviar.page.dart
+++ lib/pages/esqueceuSenhaEnviar.page.dart
@@ -1,9 +1,11 @@
 //1* - FALTA CRIAR A NOTIFICAÇÃO DE QUE O EMAIL FOI ENVIADO COM SUCESSO
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 import '../components/largeButton.dart';
 import '../components/pageTitle.dart';
+import 'package:email_validator/email_validator.dart';
+
 
 class EsqueceuSenhaPage extends StatefulWidget {
   const EsqueceuSenhaPage({Key? key}) : super(key: key);
@@ -15,6 +17,35 @@ class EsqueceuSenhaPage extends StatefulWidget {
 }
 
 class _EsqueceuSenhaPageState extends State<EsqueceuSenhaPage> {
+  final _emailController = TextEditingController();
+
+  @override
+  void dispose(){
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  Future resetPassword() async {
+
+    try {
+      await FirebaseAuth.instance
+          .sendPasswordResetEmail(email: _emailController.text.trim());
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Email enviado. Verifique sua caixa de entrada e spam'),
+          backgroundColor: Colors.blueAccent,
+        ),
+      );
+      Navigator.of(context).pushNamed('/');
+        } on FirebaseAuthException {
+      ScaffoldMessenger.of(context).showSnackBar(
+         const SnackBar(
+          content: Text('Email não cadastrado'),
+          backgroundColor: Colors.redAccent,
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +68,7 @@ class _EsqueceuSenhaPageState extends State<EsqueceuSenhaPage> {
               Container(
                 alignment: Alignment.center,
                 child: const Text(
-                  'Digite seu email para enviarmos uma nova senha',
+                  'Digite o email cadastrado',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     color: Colors.white,
@@ -57,9 +88,10 @@ class _EsqueceuSenhaPageState extends State<EsqueceuSenhaPage> {
                 ),
                 child: TextFormField(
                   //autofocus: true,
+                  controller: _emailController,
                   onChanged: (text){},
                   keyboardType: TextInputType.text,
-                  obscureText: true,
+                  obscureText: false,
                   decoration: const InputDecoration(
                     labelText: "Email",
                     border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(8),),),
@@ -69,6 +101,11 @@ class _EsqueceuSenhaPageState extends State<EsqueceuSenhaPage> {
                       fontSize: 18,
                     ),
                   ),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (email) =>
+                    email != null && !EmailValidator.validate(email)
+                        ? 'Digite um email válido'
+                        : null,
                   style: const TextStyle(
                     fontSize: 18,
                     color: Colors.white,
@@ -80,7 +117,7 @@ class _EsqueceuSenhaPageState extends State<EsqueceuSenhaPage> {
               LargeButton(
                 texto: 'Enviar',
                 onPressed: () {
-                  //1*
+                  resetPassword();
                 },
               ),
 
@@ -88,5 +125,6 @@ class _EsqueceuSenhaPageState extends State<EsqueceuSenhaPage> {
           ),
         ),
     );
+
   }
 }
